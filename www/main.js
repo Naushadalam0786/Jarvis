6f9d9b348f3df79fd8b1ceb7bc8abf348a0e5a39
Naushad <nosu7903@gmail.com>
2024-10-07 23:49:01 +0530

    Listening, Recognizing With Open and PlayYoutube Commands Complete

--- www/main.js
+++ www/main.js
@@ -1,14 +1,45 @@
 $(document).ready(function () {
+  $(".text").textillate({
+    loop: true,
+    sync: true,
+    in: {
+      effect: "bounceIn",
+    },
+    out: {
+      effect: "bounceOut",
+    },
+  });
 
-    $('.text').textillate({
-        loop: true,
+  // Siri configuration
+  var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 800,
+    height: 200,
+    style: "ios9",
+    amplitude: "1",
+    speed: "0.30",
+    autostart: true
+  });
+
+  // Siri message animation 
+    $(".siri-message").textillate({
+      loop: true,
+      sync: true,
+      in: {
+        effect: "fadeInUp",
         sync: true,
-        in: {
-            effect: "bounceIn"
-        },
-        out: {
-            effect: "bounceOut"
-        }
-    }) 
+      },
+      out: {
+        effect: "fadeOutUp",
+        sync: true
+      },
+    });
 
+    // Mic button click event
+    $("#MicBtn").click(function () { 
+      eel.playAssistantSound()
+      $("#Oval").attr("hidden", true);
+      $("#SiriWave").attr("hidden", false);
+      eel.allCommands()()
+    });
 });
